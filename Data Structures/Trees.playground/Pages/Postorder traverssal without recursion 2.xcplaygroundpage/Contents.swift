7075a11aaf69aacf911df40ba193e2e80107942f
Sean <s.christopherjones@gmail.com>
2021-06-23 14:40:23 -0400

    post order traversal without recursion code working

--- Data Structures/Trees.playground/Pages/Postorder traverssal without recursion 2.xcplaygroundpage/Contents.swift
+++ Data Structures/Trees.playground/Pages/Postorder traverssal without recursion 2.xcplaygroundpage/Contents.swift
@@ -21,36 +21,36 @@ root.left_ptr?.right_ptr = TreeNode(val: 500)
 
 
 
-func postOrder(root: TreeNode) -> [Int]? {
+func PostOrder(root: TreeNode?) -> [Int]? {
     print("hello")
     var stack = Stack<TreeNode>()
     var result = [Int]()
     guard let root = root else {
         return result
     }
-    stack.push(root)
+    stack.push(element: root)
     var prev:TreeNode? = nil
     
     repeat {
         var current: TreeNode? = stack.peek()
         
-        if prev == nil || prev!.left_ptr == current || prev!.right_ptr == current {
+        if prev == nil || prev?.left_ptr?.val == current!.val || prev?.right_ptr?.val == current!.val {
             if current?.left_ptr != nil {
-                stack.push(current!.left_ptr!)
+                stack.push(element: current!.left_ptr!)
             } else if current!.right_ptr != nil {
-                stack.push(current!.right_ptr!)
+                stack.push(element: current!.right_ptr!)
             } else {
                 stack.pop()
                 result.append(current!.val)
             }
-        } else if current!.left_ptr! == prev {
+        } else if current!.left_ptr!.val == prev!.val {
             if current!.right_ptr != nil {
-                stack.push(current!.right_ptr!)
+                stack.push(element: current!.right_ptr!)
             } else {
                 stack.pop()
                 result.append(current!.val)
             }
-        } else if current!.right_ptr == prev {
+        } else if current!.right_ptr!.val == prev!.val {
             stack.pop()
             result.append(current!.val)
         }
@@ -59,7 +59,21 @@ func postOrder(root: TreeNode) -> [Int]? {
     return result
 }
 
-postorder(root:root)
+PostOrder(root:root)
+
+/*
+ ALGORITH EXPLANATION
+ 
+ 1.1 Create an empty stack
+ 2.1 Do the Following while root is not  nil
+    - push root's right child onto the stack and then push root onto the stack
+    - set root's left child as root
+ 2.2 Pop an item from the stack andd set it as root
+    - If the popped item has a right child and the right child is at the top of the stack, then remove the child from the stack, push the root back onto the stack, and set the root as the root's right child.
+    - Else set the root to nil
+ 2.3 repeat steps 2.1 and 2.2 while stack is not empty.
+ 
+ **/
 
 
 
