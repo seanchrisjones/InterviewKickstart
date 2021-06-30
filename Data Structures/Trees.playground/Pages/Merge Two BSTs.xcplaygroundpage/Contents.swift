e2f261a9c630681826b4ca1e52c3903db9c2be8c
Sean <s.christopherjones@gmail.com>
2021-06-30 12:19:25 -0400

    pass all cases for merge 2 bsts

--- Data Structures/Trees.playground/Pages/Merge Two BSTs.xcplaygroundpage/Contents.swift
+++ Data Structures/Trees.playground/Pages/Merge Two BSTs.xcplaygroundpage/Contents.swift
@@ -34,8 +34,69 @@ bstRoot.left_ptr?.right_ptr = TreeNode(val: 55)
 bstRoot.right_ptr?.left_ptr = TreeNode(val: 125)
 bstRoot.right_ptr?.right_ptr  = TreeNode(val:157)
 
+var globalBox = [TreeNode]()
 
-func merge_two_BSTs( root1: TreeNode?, rootTwo: TreeNode?) -> TreeNode? {
+
+func merge_two_BSTs( root1: TreeNode?, root2: TreeNode?) -> TreeNode? {
+    guard let root1 = root1 else { return nil }
+    guard let root2 = root2 else { return nil }
+    dfs(node: root1)
+    dfs(node: root2)
+    globalBox.sort { TreeNode1, TreeNode2 in
+        TreeNode1.val < TreeNode2.val
+    }
+    
+    if globalBox.count == 0 {
+        return nil
+    }
+    
+    return construct(nodes: globalBox, left: 0, right: globalBox.count-1)
+    
     
 }
+
+func dfs(node: TreeNode?) {
+    guard let node = node else {return}
+    
+    
+    if node.left_ptr == nil && node.right_ptr == nil {
+        globalBox.append(node)
+    }
+    
+   
+    if let left = node.left_ptr {
+       
+        dfs(node: left)
+    }
+    
+    if let right = node.right_ptr {
+        
+        dfs(node: right)
+    }
+}
+
+
+
+func construct(nodes: [TreeNode], left: Int, right: Int) -> TreeNode? {
+    if left > right {
+        return nil
+    }
+    
+    let mid = left + (right - left) / 2
+    var tnode = TreeNode(val: nodes[mid].val)
+    tnode.left_ptr = construct(nodes: nodes, left: left, right: mid-1)
+    tnode.right_ptr = construct(nodes: nodes, left: mid+1, right: right)
+    
+    return tnode
+    
+}
+
+
+
+merge_two_BSTs(root1: root, root2: bstRoot)
+
+
+
+
+
 //: [Next](@next)
