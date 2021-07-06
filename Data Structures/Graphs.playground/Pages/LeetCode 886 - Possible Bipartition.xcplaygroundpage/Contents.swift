1a07517a8dc36c483072a632acb0a3a3578adc7e
Sean <s.christopherjones@gmail.com>
2021-07-06 18:11:47 -0400

    add code for possible biparititon

--- Data Structures/Graphs.playground/Pages/LeetCode 886 - Possible Bipartition.xcplaygroundpage/Contents.swift
+++ Data Structures/Graphs.playground/Pages/LeetCode 886 - Possible Bipartition.xcplaygroundpage/Contents.swift
@@ -10,8 +10,76 @@ import Foundation
  
  **/
 
-func possibleBipartition(_n : Int, _ dislikes: [[Int]]) -> Bool {
+func possibleBipartition(_ n : Int, _ dislikes: [[Int]]) -> Bool {
     
+    var graph = [[Int]]()
+    
+    for i in 0..<n {
+        graph.append([])
+    }
+    
+    for i in dislikes {
+        let from = i[0]-1
+        let to = i[1]-1
+        graph[from].append(to)
+        graph[to].append(from)
+    }
+    var visited = Array(repeating: 0, count: graph.count)
+    for i in 0..<n {
+        if visited[i] == 0 {
+            if BFS(i, graph, &visited) == false {
+                return false
+            }
+            
+        }
+    }
+    return true
 }
+
+func BFS(_ node: Int, _ graph: [[Int]], _ visited: inout [Int]) -> Bool {
+    var q = [Int]()
+    q.append(node)
+    visited[node] = 1
+    
+    while !q.isEmpty {
+        let item = q.removeFirst()
+        for child in graph[item] {
+            if visited[child] == 0 {
+                visited[child] = visited[item] == 1 ? 2 : 1
+                print(visited[child] = visited[item] == 1 ? 2 : 1)
+                q.append(child)
+            } else {
+                if visited[child] == visited[item] {
+                    return false
+                }
+            }
+        }
+        
+    }
+    return true
+}
+
+func DFS(_ node: Int,_ graph: [[Int]],_ visited: inout [Int],_ parent: Int? = nil) -> Bool {
+    if parent == nil{
+        visited[node] = 1
+    }
+    
+    for child in graph[node]{
+        if visited[child] == 0 {
+            visited[child] = visited[node] == 1 ? 2 : 1
+            if !DFS(child, graph, &visited) {
+                return false
+            }
+        } else {
+            if visited[child] == visited[node] {
+                return false
+            }
+        }
+    }
+    
+    return true
+}
+
+possibleBipartition(4, [[1,2],[1,3],[2,4]])
 
 //: [Next](@next)
